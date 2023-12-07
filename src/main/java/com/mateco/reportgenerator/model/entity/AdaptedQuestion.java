bc9f6f042d4773016f5c6c3ecdd76d5899a81b5b
SilmarNascimento <silmarfnascimento@gmail.com>
2023-12-07 17:04:21 -0300

    feat: implementa o relacionamento 1:N entre MainQuestion e AdaptedQuestion

--- src/main/java/com/mateco/reportgenerator/model/entity/AdaptedQuestion.java
+++ src/main/java/com/mateco/reportgenerator/model/entity/AdaptedQuestion.java
@@ -1,9 +1,12 @@
 package com.mateco.reportgenerator.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.List;
 import java.util.UUID;
@@ -20,8 +23,17 @@ public class AdaptedQuestion {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private UUID id;
+
   private String title;
+
   private String image;
+
   private List<Alternative> alternatives;
+
   private Alternative answer;
+
+  @ManyToOne
+  @JoinColumn(name = "main_question_id")
+  @JsonIgnore
+  private MainQuestion mainQuestion;
 }
--- src/main/java/com/mateco/reportgenerator/model/entity/MainQuestion.java
+++ src/main/java/com/mateco/reportgenerator/model/entity/MainQuestion.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
 import java.util.UUID;
@@ -21,13 +23,23 @@ public class MainQuestion {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private UUID id;
+
   private String title;
+
+  @ManyToMany
   private List<Subject> contents;
+
   private String image;
+
   private List<Alternative> alternatives;
+
   private Alternative answer;
+
   @Column(name = "adapted_question")
+  @OneToMany(mappedBy = "mainQuestion")
   private List<AdaptedQuestion> adaptedQuestions;
+
   private List<MockExam> mockExams;
+
   private List<Handout> handout;
 }
