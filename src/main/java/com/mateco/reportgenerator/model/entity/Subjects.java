59d2f9a6fb6e79572605004ba9b221cf8b744cef
SilmarNascimento <silmarfnascimento@gmail.com>
2023-12-07 12:15:20 -0300

    feat: implementa a entidade Subject

---
+++ src/main/java/com/mateco/reportgenerator/model/entity/Subjects.java
@@ -0,0 +1,24 @@
+package com.mateco.reportgenerator.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "subjects")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Subjects {
+  @Id
+  @GeneratedValue
+  private UUID id;
+  private String name;
+  private List<MainQuestion> mainQuestions;
+}
