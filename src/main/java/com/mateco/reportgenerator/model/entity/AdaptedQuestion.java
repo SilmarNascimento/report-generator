c4439b8c9fc0310343b95cfdaae4414837aab05d
SilmarNascimento <silmarfnascimento@gmail.com>
2023-12-07 11:37:16 -0300

    feat: inicializa o projeto e adiciona as entidades AdaptedQuestion e MainQuestion

---
+++ src/main/java/com/mateco/reportgenerator/model/entity/AdaptedQuestion.java
@@ -0,0 +1,27 @@
+package com.mateco.reportgenerator.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "adapted_questions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdaptedQuestion {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private UUID id;
+  private String title;
+  private String image;
+  private List<> alternatives;
+  private String answer;
+}
---
+++ src/main/java/com/mateco/reportgenerator/model/entity/MainQuestion.java
@@ -0,0 +1,33 @@
+package com.mateco.reportgenerator.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "main_questions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MainQuestion {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private UUID id;
+  private String title;
+  private List<> contents;
+  private String image;
+  private List<> alternatives;
+  private String answer;
+  @Column(name = "adapted_question")
+  private List<> adaptedQuestions;
+  private List<> mockExams;
+  private List<> handout;
+}
