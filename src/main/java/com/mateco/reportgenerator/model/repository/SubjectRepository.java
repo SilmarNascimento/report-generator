5f402442c43ef96b37549b8b823b637f122fe8d7
SilmarNascimento <silmarfnascimento@gmail.com>
2023-12-07 12:59:52 -0300

    feat: implementa o repository da entidade Subject com seu javadoc

---
+++ src/main/java/com/mateco/reportgenerator/model/repository/SubjectRepository.java
@@ -0,0 +1,15 @@
+package com.mateco.reportgenerator.model.repository;
+
+import com.mateco.reportgenerator.model.entity.Subject;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository - assinatura dos métodos CRUD para a entidade
+ *              subject.
+ */
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, UUID> {
+
+}
