e09813d3bb087269565ce8ea3fec1ffa4cb7ebb9
SilmarNascimento <silmarfnascimento@gmail.com>
2024-02-14 18:03:08 -0300

    WIP: adiciona um método para retornar o gabarito beseado na lista de questões principais da entidade MockExam

--- src/main/java/com/mateco/reportgenerator/model/entity/MockExam.java
+++ src/main/java/com/mateco/reportgenerator/model/entity/MockExam.java
@@ -14,9 +14,11 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.MapKeyColumn;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -59,22 +61,63 @@ public class MockExam {
   @Column(name="answer_value")
   private Map<Integer, String> answers;
 
-  public MockExam(String name, List<String> className, int number) {
+  public MockExam(
+      String name,
+      List<String> className,
+      List<Subject> subjects,
+      int number,
+      List<MainQuestion> mockExamQuestions,
+      Map<Integer, String> answers
+  ) {
     this.name = name;
     this.className = className;
+    this.subjects = subjects;
     this.number = number;
+    this.mockExamQuestions = mockExamQuestions;
+    this.answers = answers;
   }
 
   public static MockExam parseMockExam(MockExamInputDto examInputDto) {
     return new MockExam(
         examInputDto.name(),
         examInputDto.className(),
-        examInputDto.number()
+        new ArrayList<>(),
+        examInputDto.number(),
+        new ArrayList<>(),
+        new HashMap<>()
     );
   }
 
-  public List<Alternative> generateAnswers() {
+  public Map<Integer, String> generateAnswers() {
+    List<MainQuestion> questions = this.mockExamQuestions;
 
-    return new ArrayList<>();
+    final int[] initialKey = {136};
+
+    return questions.stream()
+        .collect(Collectors.toMap(
+            question -> initialKey[0]++,
+            question -> generateAlternativeLetter(question)
+        ));
   };
+
+  private String generateAlternativeLetter(MainQuestion question) {
+    List<Alternative> alternativeList = question.getAlternatives();
+
+    int index = 0;
+    for (Alternative alternative: alternativeList) {
+      if (alternative.isQuestionAnswer()) {
+        break;
+      }
+      index ++;
+    }
+
+    Map<Integer, String> map = new HashMap<>();
+    map.put(0, "A");
+    map.put(1, "B");
+    map.put(2, "C");
+    map.put(3, "D");
+    map.put(4, "E");
+
+    return map.getOrDefault(index, "");
+  }
 }
