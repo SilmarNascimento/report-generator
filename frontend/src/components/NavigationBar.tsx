5455bd21cff7232e05ef4f906d51de6b0365f32e
SilmarNascimento <silmarfnascimento@gmail.com>
2024-03-02 17:20:24 -0300

    feat: refatora components da NavigationBar

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,12 +1,17 @@
-import { ListVideo, Tags, Settings, Code2 } from 'lucide-react'
+import { Book, ScrollText, Tags, Settings} from 'lucide-react'
 
-export function Tabs() {
+export function NavigationBar() {
   return (
     <div className="border-b border-zinc-800 py-4">
       <nav className="flex items-center gap-2 max-w-[1200px] mx-auto">
         <a href="" className="py-1.5 px-3 bg-zinc-800 text-zinc-100 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent">
-          <ListVideo className="size-4" />
-          Uploads
+          <Book className="size-4" />
+          Simulados
+        </a>
+
+        <a href="" className="py-1.5 px-3 bg-zinc-800 text-zinc-100 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent">
+          <ScrollText className="size-4" />
+          Questões
         </a>
 
         <a href="" className="py-1.5 px-3 text-zinc-300 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent hover:border-zinc-800">
@@ -17,11 +22,6 @@ export function Tabs() {
         <a href="" className="py-1.5 px-3 text-zinc-300 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent hover:border-zinc-800">
           <Settings className="size-4" />
           Settings
-        </a>
-
-        <a href="" className="py-1.5 px-3 text-zinc-300 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent hover:border-zinc-800">
-          <Code2 className="size-4" />
-          Developers
         </a>
       </nav>
     </div>
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,6 +1,5 @@
 import { ChevronDown } from 'lucide-react'
 
-import nivoLogo from '../assets/logo-nivo.svg'
 import { Badge } from './ui/badge'
 
 export function Header() {
@@ -8,8 +7,6 @@ export function Header() {
     <div className="max-w-[1200px] mx-auto flex items-center justify-between">
       <div className="flex items-center gap-3">
         <div className="flex items-center gap-2.5">
-          <img src={nivoLogo} alt="nivo.video" />
-
           <Badge>BETA</Badge>
         </div>
 
