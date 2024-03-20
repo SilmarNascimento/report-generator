2263bb7d4d4d99f8cba99ddbcf6f47d820733f01
SilmarNascimento <silmarfnascimento@gmail.com>
2024-03-20 12:12:19 -0300

    feat: adiciona o component SelectForm

---
+++ frontend/src/components/ui/selectForm.tsx
@@ -0,0 +1,42 @@
+import { useFormContext } from 'react-hook-form';
+import { ChangeEvent } from 'react';
+
+interface SelectProps {
+  defaultValue?: string;
+}
+
+export function Select ({ defaultValue }: SelectProps) {
+  const { register, setValue } = useFormContext();
+
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setValue("level", event.target.value);
+  };
+
+  return (
+    <select
+      {...register('level')}
+      defaultValue={defaultValue ?? "Nível da Questão"}
+      onChange={handleChange}
+      className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-50/50 w-72 text-sm"
+    >
+      <option
+        value="Fácil"
+        className='text-zinc-800 hover:bg-gray-200'
+      >
+        Fácil
+      </option>
+      <option
+        value="Médio"
+        className='text-zinc-800'
+      >
+        Médio
+      </option>
+      <option
+        value="Difícil"
+        className='text-zinc-800'
+      >
+        Difícil
+      </option>
+    </select>
+  );
+}
