509d1e3c21b5e3aa13fec18436e818b68dbabcac
SilmarNascimento <silmarfnascimento@gmail.com>
2026-02-16 13:28:52 -0300

    feat: adiciona barra de navegação às páginas de estudantes

--- frontend/src/pages/student/EditStudent.tsx
+++ frontend/src/pages/student/EditStudent.tsx
@@ -1,4 +1,5 @@
 import StudentForm from "@/components/forms/student/StudentForm";
+import { NavigationBar } from "@/components/NavigationBar";
 import { useHandleEditStudent } from "@/hooks/CRUD/student/useHandleEditStudent";
 import { studentQueryOptions } from "@/loader/studentLoader";
 import { mapStudentResponseToForm } from "@/mapper/student";
@@ -11,20 +12,26 @@ const EditStudent = () => {
   const { data: aluno } = useSuspenseQuery(studentQueryOptions(id ?? ""));
   const { handleEdit } = useHandleEditStudent(id ?? "");
   return (
-    <main className="max-w-6xl mx-auto space-y-5">
-      <h1
-        className={`my-6 text-xl leading-[1.4] font-bold tracking-[-0.25px] text-[#2C2E34]`}
-      >
-        Alunos
-      </h1>
+    <>
+      <header>
+        <NavigationBar />
+      </header>
 
-      <StudentForm
-        modo="edicao"
-        titulo="Editar Cadastro de Aluno"
-        defaultValues={mapStudentResponseToForm(aluno)}
-        handleSubmitRequest={handleEdit}
-      />
-    </main>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <h1
+          className={`my-6 text-xl leading-[1.4] font-bold tracking-[-0.25px] text-[#2C2E34]`}
+        >
+          Alunos
+        </h1>
+
+        <StudentForm
+          modo="edicao"
+          titulo="Editar Cadastro de Aluno"
+          defaultValues={mapStudentResponseToForm(aluno)}
+          handleSubmitRequest={handleEdit}
+        />
+      </main>
+    </>
   );
 };
 
--- frontend/src/pages/student/StudentView.tsx
+++ frontend/src/pages/student/StudentView.tsx
@@ -1,4 +1,5 @@
 import StudentForm from "@/components/forms/student/StudentForm";
+import { NavigationBar } from "@/components/NavigationBar";
 import { studentQueryOptions } from "@/loader/studentLoader";
 import { mapStudentResponseToForm } from "@/mapper/student";
 import { useSuspenseQuery } from "@tanstack/react-query";
@@ -9,19 +10,27 @@ const StudentView = () => {
 
   const { data: aluno } = useSuspenseQuery(studentQueryOptions(id ?? ""));
   return (
-    <main className="max-w-6xl mx-auto space-y-5">
-      <h1
-        className={`my-6 text-xl leading-[1.4] font-bold tracking-[-0.25px] text-[#2C2E34]`}
-      >
-        Alunos
-      </h1>
+    <>
+      <header>
+        <NavigationBar />
+      </header>
 
-      <StudentForm
-        modo="view"
-        titulo="Editar Cadastro de Aluno"
-        defaultValues={mapStudentResponseToForm(aluno)}
-      />
-    </main>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <NavigationBar />
+
+        <h1
+          className={`my-6 text-xl leading-[1.4] font-bold tracking-[-0.25px] text-[#2C2E34]`}
+        >
+          Alunos
+        </h1>
+
+        <StudentForm
+          modo="view"
+          titulo="Editar Cadastro de Aluno"
+          defaultValues={mapStudentResponseToForm(aluno)}
+        />
+      </main>
+    </>
   );
 };
 
