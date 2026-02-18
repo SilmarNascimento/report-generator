7d6765b7cc05f41374bcc3a057ade481fe8a16c0
Klinke <guiklinke98@gmail.com>
2026-02-18 13:02:54 -0300

    feat: adiciona novo valor de ClassGroup

--- backend/src/main/java/com/mateco/reportgenerator/enums/ClassGroup.java
+++ backend/src/main/java/com/mateco/reportgenerator/enums/ClassGroup.java
@@ -2,8 +2,9 @@ package com.mateco.reportgenerator.enums;
 
 public enum ClassGroup {
     SIMULADO_PRESENCIAL,
-    EXTENSIVO_ONLINE,
     SIMULADO_ONLINE,
+    EXTENSIVO_PRESENCIAL,
+    EXTENSIVO_ONLINE,
     INTENSIVO_PRESENCIAL,
     INTENSIVO_ONLINE
 }
